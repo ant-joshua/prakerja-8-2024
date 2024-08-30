b694caf3fc7959548c9dc4915a78209c74c38f8f
Antonius Joshua <antoniusjoshua47@gmail.com>
2024-08-30 20:58:32 +0700

    feat: update redis

--- ecommerce/database/redis.go
+++ ecommerce/database/redis.go
@@ -1,12 +1,20 @@
 package database
 
-import "github.com/redis/go-redis/v9"
+import (
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
+)
 
 func NewRedis(dbnumber int) *redis.Client {
+
+	redisAddr := fmt.Sprintf("%s:%s", viper.GetString("REDIS_HOST"), viper.GetString("REDIS_PORT"))
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",       // no password set
-		DB:       dbnumber, // use default DB
+		Addr:     redisAddr,
+		Password: viper.GetString("REDIS_PASS"), // no password set
+		DB:       dbnumber,                      // use default DB
 	})
 
 	return rdb
